4bd2a19eeb6661dd96d7a67787496f1727e5a10c
noumansafi <noumansafi2216@gmail.com>
2022-12-26 10:57:12 +0500

    string interploation

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -1,11 +1,14 @@
 import { Component } from "@angular/core";
 
-@Component({    // this is a decorator to tell file that this is component file
-    // add metaData to component
-    selector: 'app-server', // it should be unique
-    templateUrl: './server.component.html' // template file and add it's relative path
+@Component({
+    selector: 'app-server',
+    templateUrl: './server.component.html'
 }) 
-// it is necessary to expoet it becasue it will be used by appComponent (rootComponent)
 export class ServerComponent {
+    serverId: number = 10;
+    serverStatus: string = 'offline';
 
+    serverMethod(){
+        return this.serverStatus = 'offline';
+    }
 }
