6b15699ed4e2e8d4e597f6c1fc0c8e2ae08a103b
noumansafi <noumansafi2216@gmail.com>
2022-12-26 12:24:33 +0500

    data binding using property

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,26 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  // selector: '[app-servers]', // use as property
-  // selector: '.app-servers', // use as class
   selector: 'app-servers',
-  // templateUrl: './servers.component.html', // using external template file
-  // template for internal html
-  template: `   
-    <app-server></app-server>
-    <app-server></app-server>
-  `,
-  // styleUrls: ['./servers.component.css'] // using external css file it could be mulitiple
-  // internal styling 
-  styles: [`
-    h3 {
-      color: dodgerblue;
-    }
-  `] 
+  templateUrl: './servers.component.html',
+  styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
-
-  constructor() { }
+  addNewServer: boolean = false;
+  constructor() { 
+    setTimeout(() => {
+      this.addNewServer = true;
+    },2000)
+  }
 
   ngOnInit(): void {
   }
