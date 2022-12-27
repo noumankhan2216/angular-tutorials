6eac98d238c7ca5aa284c23bf3a5fd12076c0c13
noumansafi <noumansafi2216@gmail.com>
2022-12-27 08:14:45 +0500

    ngFor directive used in servers component

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -10,6 +10,7 @@ export class ServersComponent implements OnInit {
   serverCreationStatus: string = 'No server created';
   serverName: string = '';
   serverCreated: boolean = false;
+  servers: any[] = ['testServer 1','testServer 12']
   constructor() { 
     setTimeout(() => {
       this.addNewServer = true;
@@ -20,6 +21,7 @@ export class ServersComponent implements OnInit {
   }
   onServerCreation(){
     this.serverCreated = true;
+    this.servers.push(this.serverName);
     this.serverCreationStatus = 'Server was created! Name is: '+ this.serverName;
   }
   onUpdateServerName(event: Event){
