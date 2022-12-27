6f50b5c6fa886b67d8f1b5dc5feb3f8ac786a032
noumansafi <noumansafi2216@gmail.com>
2022-12-27 06:37:42 +0500

    ngStyle directive for online offline status of server

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -7,8 +7,14 @@ import { Component } from "@angular/core";
 export class ServerComponent {
     serverId: number = 10;
     serverStatus: string = 'offline';
+    constructor(){
+        this.serverStatus = Math.random() > 0.5 ? 'online' : 'offline';
+    }
 
     serverMethod(){
-        return this.serverStatus = 'offline';
+        return this.serverStatus;
+    }
+    getColor(){
+        return this.serverStatus === 'online' ? 'green' : 'red';
     }
 }
