c8386139c7c5c57852f6a30e3202efb32f032c41
noumansafi <noumansafi2216@gmail.com>
2023-01-01 06:02:41 +0500

    split appComponents into multiple components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ServerComponent } from './server/server.component';
-import { ServersComponent } from './servers/servers.component';
+import { CockpitComponent } from './cockpit/cockpit.component';
+import { ServerElementComponent } from './server-element/server-element.component';
 @NgModule({
   declarations: [
     AppComponent,
-    ServerComponent,
-    ServersComponent,
+    CockpitComponent,
+    ServerElementComponent,
   ],
   imports: [
     BrowserModule,
