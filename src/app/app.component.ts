b7b4d0380ecc46010eb712a691c8dd6cd2f626d6
noumansafi <noumansafi2216@gmail.com>
2023-02-01 08:02:52 +0500

    on seeing lifecycle hooks in action

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -22,4 +22,10 @@ export class AppComponent {
       content: serverData.serverContent
     });
   }
+  onChangesFirst(){
+    this.serverElements[0].name = 'Changed !';
+  }
+  onDestroyFirst(){
+    this.serverElements.splice(0,1);
+  }
 }
