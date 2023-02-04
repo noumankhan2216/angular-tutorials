027458c6b1ae69e6bb5e7afce8a3fd88d2a78fa1
nouman-oneclout <nouman.khan@lhr.oneclout.com>
2023-02-04 16:01:26 +0500

    odd even game assignment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,29 +3,17 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  serverElements = [{type: 'server', name: 'testserver', content: 'This is a Server'}];
-
-  onServerAdded(serverData: {serverName: string, serverContent: string}) {
-    this.serverElements.push({
-      type: 'server',
-      name: serverData.serverName,
-      content: serverData.serverContent
-    });
-  }
-  onBlueprintAdded(serverData: {serverName: string, serverContent: string}) {
-    this.serverElements.push({
-      type: 'blueprint',
-      name: serverData.serverName,
-      content: serverData.serverContent
-    });
-  }
-  onChangesFirst(){
-    this.serverElements[0].name = 'Changed !';
-  }
-  onDestroyFirst(){
-    this.serverElements.splice(0,1);
+  oddNumbers: number[] = [];
+  evenNumbers: number[] = [];
+  onIntervalFired(firedNumber: number) {
+    console.log(firedNumber);
+    if (firedNumber % 2 === 0) {
+      this.evenNumbers.push(firedNumber);
+    } else {
+      this.oddNumbers.push(firedNumber);
+    }
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,20 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CockpitComponent } from './cockpit/cockpit.component';
-import { ServerElementComponent } from './server-element/server-element.component';
+import { GameControlComponent } from './game-control/game-control.component';
+import { OddComponent } from './odd/odd.component';
+import { EvenComponent } from './even/even.component';
 @NgModule({
   declarations: [
     AppComponent,
-    CockpitComponent,
-    ServerElementComponent,
+    GameControlComponent,
+    OddComponent,
+    EvenComponent,
   ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, FormsModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
