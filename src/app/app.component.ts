c988dc33082b25ee057ba5ad36836236b3daeceb
nouman-oneclout <nouman.khan@lhr.oneclout.com>
2023-02-08 08:38:51 +0500

    ngIf and ngFor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,14 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  oddNumbers: number[] = [];
-  evenNumbers: number[] = [];
-  onIntervalFired(firedNumber: number) {
-    console.log(firedNumber);
-    if (firedNumber % 2 === 0) {
-      this.evenNumbers.push(firedNumber);
-    } else {
-      this.oddNumbers.push(firedNumber);
-    }
-  }
+  numbers = [1, 2, 3, 4, 5, 6];
+  oddNumbers = [1, 3, 5];
+  evenNumbers = [2, 4, 6];
+  onlyOdd = false;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { GameControlComponent } from './game-control/game-control.component';
-import { OddComponent } from './odd/odd.component';
-import { EvenComponent } from './even/even.component';
 @NgModule({
-  declarations: [
-    AppComponent,
-    GameControlComponent,
-    OddComponent,
-    EvenComponent,
-  ],
+  declarations: [AppComponent],
   imports: [BrowserModule, FormsModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
