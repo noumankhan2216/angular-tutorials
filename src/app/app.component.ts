5e96d8db117d9aaaa603ab3be1bd0d49de566dd1
nouman-oneclout <nouman.khan@lhr.oneclout.com>
2023-02-14 09:10:53 +0500

    assignment of service module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,14 @@
 import { Component, OnInit } from '@angular/core';
-import { AccountsService } from './accounts.service';
+import { UserService } from './users.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  // providers: [AccountsService],
+  providers: [UserService],
 })
 export class AppComponent implements OnInit {
-  accounts: { name: string; status: string }[] = [];
-  constructor(private accountsService: AccountsService) {}
+  constructor() {}
 
-  ngOnInit(): void {
-    this.accounts = this.accountsService.accounts;
-  }
+  ngOnInit(): void {}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,14 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { AccountComponent } from './account/account.component';
-import { NewAccountComponent } from './new-account/new-account.component';
-import { AccountsService } from './accounts.service';
-import { LoggingService } from './logging.service';
+import { ActiveUsersComponent } from './active-users/active-users.component';
+import { InactiveUsersComponent } from './inactive-users/inactive-users.component';
+import { CounterService } from './counter.service';
 
 @NgModule({
-  declarations: [AppComponent, AccountComponent, NewAccountComponent],
+  declarations: [AppComponent, ActiveUsersComponent, InactiveUsersComponent],
   imports: [BrowserModule, FormsModule],
-  providers: [AccountsService, LoggingService],
+  providers: [CounterService],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
