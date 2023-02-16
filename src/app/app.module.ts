185e7c99197c543f94e5fddc696dcb7e26ed3a8d
nouman-oneclout <nouman.khan@lhr.oneclout.com>
2023-02-16 09:17:35 +0500

    setting up and loading routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,7 +11,13 @@ import { UserComponent } from './users/user/user.component';
 import { EditServerComponent } from './servers/edit-server/edit-server.component';
 import { ServerComponent } from './servers/server/server.component';
 import { ServersService } from './servers/servers.service';
+import { Route, RouterModule } from '@angular/router';
 
+const appRoutes: Route[] = [
+  {path: '', component: HomeComponent },
+  {path: 'users', component: UsersComponent },
+  {path: 'servers', component: ServersComponent },
+]
 @NgModule({
   declarations: [
     AppComponent,
@@ -22,9 +28,10 @@ import { ServersService } from './servers/servers.service';
     EditServerComponent,
     ServerComponent
   ],
-  imports: [
+  imports: [  
     BrowserModule,
     FormsModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [ServersService],
   bootstrap: [AppComponent]
