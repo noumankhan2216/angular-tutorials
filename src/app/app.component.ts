f58f9640d682648f1f5b0a698c3e1423e35d3616
nouman-oneclout <nouman.khan@lhr.oneclout.com>
2023-02-16 09:06:14 +0500

    setting routes project to work on

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,9 @@
-import { Component, OnInit } from '@angular/core';
-import { UserService } from './users.service';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
-  providers: [UserService],
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
-  constructor() {}
-
-  ngOnInit(): void {}
+export class AppComponent {
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,31 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
+
 import { AppComponent } from './app.component';
-import { ActiveUsersComponent } from './active-users/active-users.component';
-import { InactiveUsersComponent } from './inactive-users/inactive-users.component';
-import { CounterService } from './counter.service';
+import { HomeComponent } from './home/home.component';
+import { UsersComponent } from './users/users.component';
+import { ServersComponent } from './servers/servers.component';
+import { UserComponent } from './users/user/user.component';
+import { EditServerComponent } from './servers/edit-server/edit-server.component';
+import { ServerComponent } from './servers/server/server.component';
+import { ServersService } from './servers/servers.service';
 
 @NgModule({
-  declarations: [AppComponent, ActiveUsersComponent, InactiveUsersComponent],
-  imports: [BrowserModule, FormsModule],
-  providers: [CounterService],
-  bootstrap: [AppComponent],
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    UsersComponent,
+    ServersComponent,
+    UserComponent,
+    EditServerComponent,
+    ServerComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+  ],
+  providers: [ServersService],
+  bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
