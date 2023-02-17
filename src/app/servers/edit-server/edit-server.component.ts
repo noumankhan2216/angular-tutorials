3fc724bdeac2ed58e2c695622ef9d9d0dea6e229
nouman-oneclout <nouman.khan@lhr.oneclout.com>
2023-02-17 10:24:47 +0500

    retrieving query parameters and fragments

--- src/app/servers/edit-server/edit-server.component.ts
+++ src/app/servers/edit-server/edit-server.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 import { ServersService } from '../servers.service';
 
@@ -12,10 +13,19 @@ export class EditServerComponent implements OnInit {
   serverName: any;
   serverStatus: any;
 
-  constructor(private serversService: ServersService) { 
+  constructor(private serversService: ServersService, private rotue: ActivatedRoute) { 
   }
 
   ngOnInit() {
+    // the console section of the code will give just the snapshot of the url
+    // it will not be reactive 0
+    console.log(this.rotue.snapshot.queryParams);
+    console.log(this.rotue.snapshot.fragment);
+    // to make it reactive we would like to make it an observale 
+    // so every time there is a change component will listen to it.
+    this.rotue.queryParams.subscribe();
+    this.rotue.fragment.subscribe();
+    
     this.server = this.serversService.getServer(1);
     this.serverName = this.server?.name;
     this.serverStatus = this.server?.status;
