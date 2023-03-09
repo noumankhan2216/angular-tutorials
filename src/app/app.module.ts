b799f3f25e39600cd2757e9d0a4947825fa8a694
Usama Mahmood <usamamahmood009@gmail.com>
2023-03-09 08:17:58 +0500

    setting up child (neseted) routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,11 +15,13 @@ import { Route, RouterModule } from '@angular/router';
 
 const appRoutes: Route[] = [
   {path: '', component: HomeComponent },
-  {path: 'users', component: UsersComponent },
-  {path: 'users/:/:name', component: UserComponent},
-  {path: 'servers', component: ServersComponent },
-  {path: 'servers/:id', component: ServerComponent},
-  {path: 'servers/:id/edit', component: EditServerComponent}
+  {path: 'users', component: UsersComponent, children: [
+    {path: ':id/:name', component: UserComponent},
+  ] },
+  {path: 'servers', component: ServersComponent, children: [
+    {path: ':id', component: ServerComponent},
+    {path: ':id/edit', component: EditServerComponent}
+  ] },
 ]
 @NgModule({
   declarations: [
