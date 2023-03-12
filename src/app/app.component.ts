b41796a6be5a4fdb9c45c6938eab8b6684be4cdb
Usama Mahmood <usamamahmood009@gmail.com>
2023-03-12 19:42:12 +0500

    adding subjects

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { UserService } from './user.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
-  constructor() {}
+export class AppComponent implements OnInit, OnDestroy {
+  userActivated = false;
+  activatedSub!: Subscription;
+  constructor(private userService: UserService) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.activatedSub = this.userService.activatedEmitter.subscribe((didActivated: boolean) => {
+      this.userActivated = didActivated;
+    })
+  }
+  ngOnDestroy(): void {
+      this.activatedSub.unsubscribe();
+  }
 }
