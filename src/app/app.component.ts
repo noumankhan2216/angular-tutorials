46f8e8e2fcf06fd321c5164468b6f3c2b1caa7c5
Usama Mahmood <usamamahmood009@gmail.com>
2023-04-01 12:54:31 +0500

    setup forms and adding controls into it

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,16 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
-import { Subscription } from 'rxjs';
-import { UserService } from './user.service';
+import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit, OnDestroy {
-  userActivated = false;
-  activatedSub!: Subscription;
-  constructor(private userService: UserService) {}
-
-  ngOnInit() {
-    this.activatedSub = this.userService.activatedEmitter.subscribe((didActivated: boolean) => {
-      this.userActivated = didActivated;
-    })
+export class AppComponent {
+  suggestUserName() {
+    const suggestedName = 'Superuser';
   }
-  ngOnDestroy(): void {
-      this.activatedSub.unsubscribe();
+  submit(){
+    console.log('submitted')
   }
 }
--- src/app/user.service.ts
+++
@@ -1,8 +0,0 @@
-import { Injectable, EventEmitter } from '@angular/core';
-import { Subject } from 'rxjs';
-
-@Injectable({providedIn: 'root'})
-export class UserService{
-    // activatedEmitter = new EventEmitter<boolean>()
-    activatedEmitter = new Subject<boolean>()
-}
