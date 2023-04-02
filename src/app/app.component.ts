d09443018cec146f8e10c9328ea22f400c1d77cd
nouman-oneclout <nouman.khan@lhr.oneclout.com>
2023-04-02 16:01:41 +0500

    formArray array of form controls

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -10,16 +10,25 @@ export class AppComponent implements OnInit{
   genders = ['male', 'female'];
   signUpForm!: FormGroup;
 
+  get hobbies(){
+    return this.signUpForm.controls['hobbies'] as FormArray;
+  }
+
   ngOnInit(): void {
       this.signUpForm = new FormGroup({
         userData: new FormGroup({
           username: new FormControl(null, Validators.required),
           email: new FormControl(null, [Validators.required, Validators.email]),
         }),
-        gender: new FormControl('female')
+        gender: new FormControl('female'),
+        hobbies: new FormArray([])
       }); 
   }
   onSubmit(){
     console.log(this.signUpForm)
+  }
+  onAddHobby(){
+    const control = new FormControl(null, Validators.required);
+    (<FormArray>this.signUpForm.get('hobbies')).push(control);
   }
 }
