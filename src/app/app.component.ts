6fb41c1cb26ec40fb78e21f8cef34afe79e0faf6
nouman-oneclout <nouman.khan@lhr.oneclout.com>
2023-04-02 16:13:24 +0500

    creating a custom validators

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,6 +9,7 @@ import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
 export class AppComponent implements OnInit{
   genders = ['male', 'female'];
   signUpForm!: FormGroup;
+  forbiddenUsernames = ['Chris', 'Anna'];
 
   get hobbies(){
     return this.signUpForm.controls['hobbies'] as FormArray;
@@ -17,7 +18,7 @@ export class AppComponent implements OnInit{
   ngOnInit(): void {
       this.signUpForm = new FormGroup({
         userData: new FormGroup({
-          username: new FormControl(null, Validators.required),
+          username: new FormControl(null, [Validators.required, this.forbiddenNames.bind(this)]),
           email: new FormControl(null, [Validators.required, Validators.email]),
         }),
         gender: new FormControl('female'),
@@ -30,5 +31,12 @@ export class AppComponent implements OnInit{
   onAddHobby(){
     const control = new FormControl(null, Validators.required);
     (<FormArray>this.signUpForm.get('hobbies')).push(control);
+  }
+
+  forbiddenNames(control: FormControl): {[s: string]: boolean}{
+    if(this.forbiddenUsernames.indexOf(control.value) ! == -1){
+      return {'nameIsForbidden': true};
+    }
+    return {'nameIsForbidden': false};
   }
 }
