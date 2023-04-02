eda9aed95de95bde11bff9638d55e468edf18bcc
nouman-oneclout <nouman.khan@lhr.oneclout.com>
2023-04-02 10:50:13 +0500

    resetting forms

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -47,5 +47,6 @@ export class AppComponent {
     this.user.secretQuestion = this.signUpForm.value.secret;
     this.user.answer = this.signUpForm.value.questionAnswer;
     this.user.gender = this.signUpForm.value.gender;
+    this.signUpForm.reset();
   }
 }
